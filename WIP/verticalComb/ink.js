382db562f242ecf85cc90ead42213707011a75be
Kathy <65121394+kfahn22@users.noreply.github.com>
2024-03-25 06:52:52 -0400

    add comb version

---
+++ WIP/verticalComb/ink.js
@@ -0,0 +1,119 @@
+const circleDetail = 1000;
+
+let colors = [
+  [18, 69, 89],
+  [89, 131, 146],
+  [174, 195, 176],
+  [239, 246, 224],
+];
+
+// Write a function to initialize multiple tines
+// function addPoints(n, sp) {
+//   let startPoints = [];
+//   let s = 0;
+//   for (let i = 0; i < n; i++) {
+//     startPoints.push(s + sp * i);
+//   }
+//   return startPoints;
+// }
+
+class Drop {
+  constructor(x, y, r) {
+    this.center = createVector(x, y);
+    this.r = r;
+    //this.sp = width/this.n;
+    this.n = 4;
+    this.z = 20;
+    this.c = 16;
+    // try to create one than one tine
+    //this.start = createVector(0, 0);
+    this.startPoints = [];
+    this.vertices = [];
+    for (let i = 0; i < circleDetail; i++) {
+      let angle = map(i, 0, circleDetail, 0, TWO_PI);
+      let v = createVector(cos(angle), sin(angle));
+      v.mult(this.r);
+      v.add(this.center);
+      this.vertices[i] = v;
+    }
+    this.vtines = [];
+    this.col = random(colors);
+  }
+
+  addPoints() {
+    let s = 0;
+    let sp = width / this.n;
+    for (let i = 0; i < this.n; i++) {
+      this.startPoints.push(s + sp * i);
+      console.log(this.startPoints[i]);
+    }
+  }
+
+  tine(m, x, y, z, c) {
+    let u = 1 / pow(2, 1 / c);
+    let b = createVector(x, y);
+    for (let v of this.vertices) {
+      let pb = p5.Vector.sub(v, b);
+      let n = m.copy().rotate(HALF_PI);
+      let d = abs(pb.dot(n));
+      let mag = z * pow(u, d);
+      v.add(m.copy().mult(mag));
+    }
+  }
+  // need to shuffle or they will be correlated
+  //  let startX = shuffle(startPoints);
+  //     let startY = shuffle(startPoints);
+  vTines() {
+    let y;
+    this.addPoints();
+    //this.startPoints = this.addPoints(this.n, this.sp);
+    for (let i = 0; i < this.n; i++) {
+      let x = this.startPoints[i];
+      console.log(x);
+      y += this.vTine(x, this.z, this.c);
+      this.vtines.push(x, y);
+    }
+  }
+
+  // vertical tine
+  vTine(x, z, c) {
+    let u = 1 / pow(2, 1 / c);
+    for (let v of this.vertices) {
+      v.x = v.x;
+      v.y = v.y + z * pow(u, abs(v.x - x));
+    }
+  }
+
+  // horizontal tine
+  hTine(y, z, c) {
+    let u = 1 / pow(2, 1 / c);
+    for (let v of this.vertices) {
+      v.x = v.x + z * pow(u, abs(v.y - y));
+      v.y = v.y;
+    }
+  }
+
+  marble(other) {
+    for (let v of this.vertices) {
+      let c = other.center;
+      let r = other.r;
+      let p = v.copy();
+      p.sub(c);
+      let m = p.mag();
+      let root = sqrt(1 + (r * r) / (m * m));
+      p.mult(root);
+      p.add(c);
+      v.set(p);
+    }
+  }
+
+  show() {
+    fill(this.col);
+    noStroke();
+    beginShape();
+    for (let v of this.vertices) {
+      vertex(v.x, v.y);
+    }
+    endShape(CLOSE);
+  }
+}
