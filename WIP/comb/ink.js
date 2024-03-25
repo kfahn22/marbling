47d1da29c05b64be941605d51fc67a85831262cb
Kathy <65121394+kfahn22@users.noreply.github.com>
2024-03-25 10:56:13 -0400

    add north and south functions

---
+++ WIP/comb/ink.js
@@ -0,0 +1,159 @@
+const circleDetail = 1000;
+
+let colors = [
+  [18, 69, 89],
+  [89, 131, 146],
+  [174, 195, 176],
+  [239, 246, 224],
+];
+
+class Drop {
+  constructor(x, y, r) {
+    this.center = createVector(x, y);
+    this.r = r;
+    //this.sp = width/this.n;
+    this.n = 4;
+    this.z = 60;
+    this.c = 8;
+    // try to create one than one tine
+    //this.start = createVector(0, 0);
+    this.startPoints = [];
+    this.vertices = [];
+    for (let i = 0; i < circleDetail; i++) {
+      let angle = map(i, 0, circleDetail, 0, TWO_PI);
+      let v = createVector(cos(angle), sin(angle));
+      v.mult(this.r);
+      v.add(this.center);
+      this.vertices[i] = v;
+    }
+    this.vtines = [];
+    this.htines = [];
+    this.col = random(colors);
+  }
+
+  // Function that adds starting points for tines
+  // Need two options - top and bottom of canvas
+  addPoints() {
+    let s;
+    let sp = width / this.n;
+    //if (dir === 0) {
+    s = 0;
+    for (let i = 0; i < this.n; i++) {
+      this.startPoints.push(s + sp * i);
+    }
+    // } else if (dir === 1) {
+    //   s = width;
+    //   for (let i = 0; i < this.n; i++) {
+    //     this.startPoints.push(s - sp * i);
+    //   }
+    //}
+  }
+
+  tine(m, x, y, z, c) {
+    let u = 1 / pow(2, 1 / c);
+    let b = createVector(x, y);
+    for (let v of this.vertices) {
+      let pb = p5.Vector.sub(v, b);
+      let n = m.copy().rotate(HALF_PI);
+      let d = abs(pb.dot(n));
+      let mag = z * pow(u, d);
+      v.add(m.copy().mult(mag));
+    }
+  }
+  // need to shuffle or they will be correlated
+  //  let startX = shuffle(startPoints);
+  //     let startY = shuffle(startPoints);
+  north() {
+    let y;
+    this.addPoints();
+    //this.startPoints = this.addPoints(this.n, this.sp);
+    for (let i = 0; i < this.n; i++) {
+      let x = this.startPoints[i];
+      y += this.vTine(x, this.z, this.c, 1);
+      this.vtines.push(x, y);
+      // console.log(this.vtines[0]);
+    }
+  }
+
+  south() {
+    let x;
+    this.addPoints();
+    //this.startPoints = this.addPoints(this.n, this.sp);
+    for (let i = 0; i < this.n; i++) {
+      let y = this.startPoints[i];
+      //console.log(x);
+      x += this.vTine(y, this.z, this.c, 0);
+      this.htines.push(x, y);
+    }
+  }
+
+  east() {
+    let x;
+    this.addPoints();
+    //this.startPoints = this.addPoints(this.n, this.sp);
+    for (let i = 0; i < this.n; i++) {
+      let y = this.startPoints[i];
+      //console.log(x);
+      x += this.hTine(y, this.z, this.c);
+      this.htines.push(x, y);
+    }
+  }
+
+  west() {
+    let x;
+    this.addPoints();
+    //this.startPoints = this.addPoints(this.n, this.sp);
+    for (let i = 0; i < this.n; i++) {
+      let y = this.startPoints[i];
+      //console.log(x);
+      x += this.hTine(y, this.z, this.c);
+      this.htines.push(x, y);
+    }
+  }
+
+  // vertical tine
+  vTine(x, z, c, dir) {
+    let u = 1 / pow(2, 1 / c);
+    for (let v of this.vertices) {
+      v.x = v.x;
+      if (dir === 0) {
+        v.y = v.y + z * pow(u, abs(v.x - x));
+      } else if (dir === 1) {
+        v.y = v.y - z * pow(u, abs(v.x - x));
+      }
+    }
+  }
+
+  // horizontal tine
+  hTine(y, z, c) {
+    let u = 1 / pow(2, 1 / c);
+    for (let v of this.vertices) {
+      v.x = v.x + z * pow(u, abs(v.y - y));
+      v.y = v.y;
+    }
+  }
+
+  marble(other) {
+    for (let v of this.vertices) {
+      let c = other.center;
+      let r = other.r;
+      let p = v.copy();
+      p.sub(c);
+      let m = p.mag();
+      let root = sqrt(1 + (r * r) / (m * m));
+      p.mult(root);
+      p.add(c);
+      v.set(p);
+    }
+  }
+
+  show() {
+    fill(this.col);
+    noStroke();
+    beginShape();
+    for (let v of this.vertices) {
+      vertex(v.x, v.y);
+    }
+    endShape(CLOSE);
+  }
+}
---
+++ WIP/comb/sketch.js
@@ -0,0 +1,87 @@
+// https://editor.p5js.org/codingtrain/sketches/fsw-rJrpr
+
+let drops = [];
+let n = 2;
+let z = 30;
+let c = 60;
+let startPoints = [];
+let endPoints = [];
+
+function setup() {
+  createCanvas(600, 600);
+  sp = width / n;
+  for (let i = 0; i < 20; i++) {
+    addInk(random(width), random(height), 100);
+    //addInk(300, 300, 100);
+  }
+
+  for (let i = 0; i < n; i++) {
+    tineLines();
+  }
+}
+
+// function addPoints(n, sp) {
+//   let s = 0;
+//   for (let i = 0; i < n; i++) {
+//     startPoints.push(s + sp * i);
+//     //startPoints.push(createVector(s + i * sp, 0));
+//   }
+// }
+// function stopPoints(n, sp) {
+//   let s = 0;
+//   for (let i = 0; i < n; i++) {
+//     endPoints.push(s + sp * i);
+//     //startPoints.push(createVector(s + i * sp, 0));
+//   }
+// }
+
+// function movePoints() {
+//   for (let i = 0; i < n; i++) {
+//     let start = startPoints[i];
+//     let e = endPoints[i];
+//     e.sub(start);
+//     e.normalize();
+//     //endPoints.push(stop);
+//     let end = createVector(end, y);
+//     tineLine(end, start, y, 2, 16);
+//   }
+// }
+
+// horizontal x of vertical line
+function tineLines() {
+  for (let drop of drops) {
+    drop.south();
+    //drop.north();
+  }
+}
+// function horizontalTines(startPoints, sp, z, c) {
+//   for (let drop of drops) {
+//     drop.hTine(x, 60, 16);
+//   }
+// }
+
+// function verticalTines(startPoints, sp, z, c) {
+//   for (let drop of drops) {
+//     drop.vTine(x, z, c);
+//   }
+// }
+
+// function mousePressed() {
+//   save("marble.jpg");
+// }
+
+function addInk(x, y, r) {
+  let drop = new Drop(x, y, r);
+  for (let other of drops) {
+    other.marble(drop);
+  }
+  drops.push(drop);
+  //console.log(drops[0].vertices[0].x);
+}
+
+function draw() {
+  background(2255, 90, 95);
+  for (let drop of drops) {
+    drop.show();
+  }
+}
